36e73f992cc38186d6c3c83a138b57a7be653be1
Mario Salinas <101608787+mojave-ghost@users.noreply.github.com>
2026-08-17 17:50:45 -0700

    add missing colons

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ print("2. Ranged")
 print("3. Spell")
 attack = input("Select your attack [1-3]: ")
 
-if (attack == 1)
+if (attack == 1):
     target_hit_points = melee(Dummy)
     print(f"You hit your target for 10 melee damage. Your target's hit points is now {target_hit_points}")
-elif (attack == 2)
+elif (attack == 2):
     target_hit_points = ranged(Dummy)
     print(f"You hit your target for 7.5 spell damage. Your target's hit points is now {target_hit_points}")
-elif (attack == 3)
+elif (attack == 3):
     target_hit_points = spell(Dummy)
     print(f"You hit your target for 5 spell damage. Your target's hit points is now {target_hit_points}")
