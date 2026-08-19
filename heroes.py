9f16b2eea523f6ba6001616cad56213760a60f8b
Mario Salinas <101608787+mojave-ghost@users.noreply.github.com>
2026-08-19 13:06:50 -0700

    add heroes

---
+++ heroes.py
@@ -0,0 +1,20 @@
+class Wombat:
+    hit_points = 100
+    tackle = 10
+    headbutt = 25
+    double_edge = 60
+    quick_attack = 17.5
+
+class Pelican:
+    hit_points = 100
+    hurricane = 15
+    scald = 25
+    wide_guard = 10
+    protect = 0
+
+class Starfish:
+    hit_points = 100
+    surf = 20
+    pyschic = 17.5
+    thunderbolt = 17.5
+    ice_beam = 17.5
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import combat
 from dummy import Dummy
+from heroes import Wombat, Pelican, Starfish
 
 print("1. Melee")
 print("2. Ranged")
 print("3. Spell")
 attack = int(input("Select your attack [1-3]: "))
+print("1. Wombat")
+print("2. Pelican")
+print("3. Starfish")
+player = int(input("Select your hero [1-3]: "))
 
 target = Dummy()
 if (attack == 1):
@@ -16,3 +21,12 @@ elif (attack == 2):
 elif (attack == 3):
     target_hit_points = combat.spell(target)
     print(f"You hit your target for 5 spell damage. Your target's hit points is now {target_hit_points}.")
+
+if player == 1:
+    hero = Wombat()
+elif player == 2:
+    hero == Pelican()
+elif player == 3:
+    hero = Starfish()
+
+print(hero)
