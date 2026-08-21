6bfc946a1f1368ff49c062b19085eba3650f753f
Mario Salinas <101608787+mojave-ghost@users.noreply.github.com>
2026-08-21 14:41:53 -0700

    set up 'select your opponent' logic

---
+++ ai_opponent.py
@@ -0,0 +1,44 @@
+import random
+
+def select_battle_move(opponent):
+    move = random.randinit(1,4)
+    if opponent == "Wombat"
+        if move == 1:
+            target.hit_points -= hero.hurricane
+            print(f"You hit your target for {hero.hurricane} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 2:
+            target.hit_points -= hero.scald
+            print(f"You hit your target for {hero.scald} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 3:
+            target.hit_points -= hero.wide_guard
+            print(f"You hit your target for {hero.wide_guard} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 4:
+            target.hitpoints -= hero.protect
+            print(f"You hit your target for {hero.protect} damage. Your target's hit points are now {target.hit_points}")
+    elif player == "Pelican":
+        if move == 1:
+            target.hit_points -= hero.hurricane
+            print(f"You hit your target for {hero.hurricane} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 2:
+            target.hit_points -= hero.scald
+            print(f"You hit your target for {hero.scald} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 3:
+            target.hit_points -= hero.wide_guard
+            print(f"You hit your target for {hero.wide_guard} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 4:
+            target.hitpoints -= hero.protect
+            print(f"You hit your target for {hero.protect} damage. Your target's hit points are now {target.hit_points}")
+    elif opponent = "Starfish":
+        if move == 1:
+            target.hit_points -= hero.hurricane
+            print(f"You hit your target for {hero.hurricane} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 2:
+            target.hit_points -= hero.scald
+            print(f"You hit your target for {hero.scald} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 3:
+            target.hit_points -= hero.wide_guard
+            print(f"You hit your target for {hero.wide_guard} damage. Your target's hit points are now {target.hit_points}")
+        elif move == 4:
+            target.hitpoints -= hero.protect
+            print(f"You hit your target for {hero.protect} damage. Your target's hit points are now {target.hit_points}")
+    
--- main.py
+++ main.py
@@ -2,30 +2,25 @@ import combat
 from dummy import Dummy
 from heroes import Wombat, Pelican, Starfish
 
-"""
-print("1. Melee")
-print("2. Ranged")
-print("3. Spell")
-attack = int(input("Select your attack [1-3]: "))
-"""
+# Select your hero
 print("1. Wombat")
 print("2. Pelican")
 print("3. Starfish")
 player = int(input("Select your hero [1-3]: "))
 target = Dummy()
+# Select your opponent
+print("1. Wombat")
+print("2. Pelican")
+print("3. Starfish")
+opponent = int(input("Select your opponent [1-3]: "))
+if opponent = 1:
+    enemy = Wombat()
+elif opponent = 2:
+    enemy = Pelican()
+elif opponent = 3: 
+    enemy = Starfish()
 
-"""
-if (attack == 1):
-    target_hit_points = combat.melee(target)
-    print(f"You hit your target for 10 melee damage. Your target's hit points is now {target_hit_points}.")
-elif (attack == 2):
-    target_hit_points = combat.ranged(target)
-    print(f"You hit your target for 7.5 spell damage. Your target's hit points is now {target_hit_points}.")
-elif (attack == 3):
-    target_hit_points = combat.spell(target)
-    print(f"You hit your target for 5 spell damage. Your target's hit points is now {target_hit_points}.")
-"""
-
+# Choose your move
 if player == 1:
     hero = Wombat()
     print("1. Tackle")
