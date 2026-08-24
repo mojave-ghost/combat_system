64efe33a95916f3d94c3fd3f9b92187c8a914920
Mario Salinas <101608787+mojave-ghost@users.noreply.github.com>
2026-08-24 14:03:49 -0700

    set up a main menu. use tuple unpacking to handle the battle() argument conflicts

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from heroes import Wombat, Pelican, Starfish
 from random_attack import random_attack
 from attack import attack
+import hero
+
 
 # Select your hero
-print("1. Wombat")
-print("2. Pelican")
-print("3. Starfish")
-player = int(input("Select your hero [1-3]: "))
-if player == 1:
-    hero = Wombat()
-elif player == 2:
-    hero = Pelican()
-elif player == 3: 
-    hero = Starfish()
-print()
+def select_hero():
+    print("1. Wombat")
+    print("2. Pelican")
+    print("3. Starfish")
+    player = int(input("Select your hero [1-3]: "))
+    if player == 1:
+        hero = Wombat()
+    elif player == 2:
+        hero = Pelican()
+    elif player == 3: 
+        hero = Starfish()
+    print()
+    # Name your hero option 
+    is_named = input("Do you want to name your hero? [y/n]")
+    if is_named == "y" or is_named == "Y":
+        name = input("Name your hero: ")
+    elif is_named.lower() == "n":
+        name = "Your hero"
 
-# Name your hero option 
-is_named = input("Do you want to name your hero? [y/n]")
-if is_named == "y" or is_named == "Y":
-    name = input("Name your hero: ")
-elif is_named.lower() == "n":
-    name = "Your hero"
+    return hero, name
 
-# Select your opponent
-print("1. Wombat")
-print("2. Pelican")
-print("3. Starfish")
-opponent = int(input("Select your opponent [1-3]: "))
-if opponent == 1:
-    enemy = Wombat()
-elif opponent == 2:
-    enemy = Pelican()
-elif opponent == 3: 
-    enemy = Starfish()
+def select_opponent():
+    # Select your opponent
+    print("1. Wombat")
+    print("2. Pelican")
+    print("3. Starfish")
+    opponent = int(input("Select your opponent [1-3]: "))
+    if opponent == 1:
+        return Wombat()
+    elif opponent == 2:
+        return Pelican()
+    elif opponent == 3: 
+        return Starfish()
 
-def main():
+def battle(hero, enemy, name):
     while True:
         attack(hero, enemy, name)
         random_attack(enemy, hero, name)    
@@ -47,5 +52,26 @@ def main():
             break
         else:
             continue
+
+def main():
+    # Start the game
+    print("****************************************")
+    print("POKEMAN: A HEART SO TRUUUUE")
+    print("****************************************")
+    print()
+    print("Main Menu")
+    print("[1] New Game")
+    print("[2] Load Game")
+    game_state = int(input("Start a new game or load saved file? [1-2]: "))
+
+    if game_state == 1:
+        hero, name = select_hero()
+        enemy = select_opponent()
+        battle(hero, enemy, name) 
+    elif game_state == 2:
+        name = hero.name
+        class_type = hero.class_type
+        print(name)
+        print(class_type)
 
 main()
