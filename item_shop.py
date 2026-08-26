dc739c53e055601b995db5e4974e6ce64d2593ae
Mario Salinas <101608787+mojave-ghost@users.noreply.github.com>
2026-08-26 10:23:37 -0700

    add item shop and shop catalog

---
+++ item_shop.py
@@ -0,0 +1,7 @@
+shop_catalog = [
+    {"name": "Potion", "id": "001", "price": 50, "description": "Heal 20 HP."},
+    {"name": "Super Potion", "id": "002", "price": 100, "description": "Heal 50 HP."},
+    {"name": "Poison Heal", "id": "003", "price": 75, "description": "Cure a poison effect."},
+    {"name": "Burn Heal", "id": "004", "price": 75, "description": "Cure a burn effect."},
+    {"name": "Awakening", "id": "005", "price": 75, "description": "Wake up a sleeping hero."}
+]
