2ca78adb8474be1d68dbc53671b2089a2fa9325c
Mario Salinas <101608787+mojave-ghost@users.noreply.github.com>
2026-08-26 10:27:25 -0700

    add new shop levels

--- item_shop.py
+++ item_shop.py
@@ -1,7 +1,21 @@
-shop_catalog = [
+shop_catalog_level1 = [
     {"name": "Potion", "id": "001", "price": 50, "description": "Heal 20 HP."},
     {"name": "Super Potion", "id": "002", "price": 100, "description": "Heal 50 HP."},
     {"name": "Poison Heal", "id": "003", "price": 75, "description": "Cure a poison effect."},
     {"name": "Burn Heal", "id": "004", "price": 75, "description": "Cure a burn effect."},
     {"name": "Awakening", "id": "005", "price": 75, "description": "Wake up a sleeping hero."}
+]
+
+shop_catalog_level2 = [
+    {"name": "Super Potion", "id": "002", "price": 100, "description": "Heal 50 HP."},
+    {"name": "Poison Heal", "id": "003", "price": 75, "description": "Cure a poison effect."},
+    {"name": "Burn Heal", "id": "004", "price": 75, "description": "Cure a burn effect."},
+    {"name": "Awakening", "id": "005", "price": 75, "description": "Wake up a sleeping hero."},
+    {"name": "Rare Candy", "id": "006", "price": 200, "description": "Increase hero level by 1."}
+]
+
+shop_catalog_level3 = [
+    {"name": "MooMoo Milk", "id": "002", "price": 100, "description": "Heal 100 HP."},
+    {"name": "Full Heal", "id": "007", "price": 150, "description": "Cure all status effects."},
+    {"name": "Rare Candy", "id": "006", "price": 200, "description": "Increase hero level by 1."}
 ]
