3e3578436e1d5e254f41a2ad65beeb50cc97534b
Mario Salinas <101608787+mojave-ghost@users.noreply.github.com>
2026-08-29 16:04:59 -0700

    loop the main menu

--- adventure_menu.py
+++ adventure_menu.py
@@ -2,10 +2,19 @@ import battle
 import items.shop_menu as shop_menu
 
 def adventure_menu(hero, name):
-    print("1. Battle \n2. Item Shop \n3. ")
-    choice = int(input("Choose your adventure [1-3]: "))
+    while True: 
+        try:
+            print("1. Battle \n2. Item Shop \n3. \n 0. Exit")
+            choice = int(input("Choose your adventure [1-3]: "))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
 
-    if choice == 1:
-        battle.battle(hero, name)
-    elif choice == 2:
-        shop_menu.shop_menu()
+        if choice == 1:
+            battle.battle(hero, name)
+        elif choice == 2:
+            shop_menu.shop_menu()
+        elif choice == 0:
+            print("Thanks for playing!")
+            break
+        else:
+            print("Please enter a valide option")
